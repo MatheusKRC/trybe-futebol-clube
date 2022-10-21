1dd6c637e9674275c987faa2014c22c09b4ce3b6
Matheus leão <kyracraft0@gmail.com>
2022-10-21 17:09:13 -0300

    [Requisito 1, 14 e 18]Criadas Migrations (Users, Matches e Teams) e a model Users

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,43 @@
+import { INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class User extends Model {
+  id: number;
+  username: string;
+  role: string;
+  email: string;
+  password: string;
+}
+
+User.init({
+  id: {
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+    type: INTEGER,
+  },
+  username: {
+    allowNull: false,
+    type: STRING,
+  },
+  role: {
+    allowNull: false,
+    type: STRING,
+  },
+  email: {
+    allowNull: false,
+    type: STRING,
+  },
+  password: {
+    allowNull: false,
+    type: STRING,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'User',
+  timestamps: false,
+});
+
+export default User;
